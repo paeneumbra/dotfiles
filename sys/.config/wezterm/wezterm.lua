05ffe851f555b3925444e681682bd3556d7035af
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-01-11 19:38:41 +0100

    WS: introduce wezterm configuration

---
+++ sys/.config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font_with_fallback({
+		{ family = "Iosevka Term", weight = "Medium" },
+		"Fira Code",
+	}),
+    font_size = 14,
+	color_scheme = "Bright (base16)",
+
+	-- Padding
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
+
+	-- Tab Bar
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	show_tab_index_in_tab_bar = true,
+
+	-- General
+	automatically_reload_config = true,
+	window_background_opacity = 0.95,
+	window_close_confirmation = "NeverPrompt",
+}
