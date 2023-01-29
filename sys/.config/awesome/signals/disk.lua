5e8901ccc21e448567770fdc8d859d39095063dd
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-01-29 16:09:02 +0100

    WS | AWESOME: Add disk signal

---
+++ sys/.config/awesome/signals/disk.lua
@@ -0,0 +1,20 @@
+local awful = require("awful")
+local gears = require("gears")
+
+local cmd_disk_usage_percentage = "df /dev/mapper/luksdev | tail -1 | awk '{print $5}' | cut -c 1"
+
+local function get_disk_space()
+	awful.spawn.easy_async_with_shell(cmd_disk_usage_percentage, function(percentage)
+		local used_percentage = tonumber(percentage)
+		awesome.emit_signal("signal::disk", used_percentage)
+	end)
+end
+
+gears.timer({
+	timout = 10000,
+	call_now = true,
+	autostart = true,
+	callback = function()
+		get_disk_space()
+	end,
+})
--- sys/.config/awesome/signals/init.lua
+++ sys/.config/awesome/signals/init.lua
@@ -1,3 +1,4 @@
 require("signals.signals")
 require("signals.volume")
 require("signals.battery")
+require("signals.disk")
