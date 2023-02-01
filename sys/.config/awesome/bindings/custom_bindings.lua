f21129a554de70075ffc08b651cd6e50940bb2c7
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-02-02 00:52:48 +0100

    WS | AWESOME: Update xinitrc, map escape to close sidebar

--- sys/.config/awesome/bindings/custom_bindings.lua
+++ sys/.config/awesome/bindings/custom_bindings.lua
@@ -1,4 +1,5 @@
 local awful = require("awful")
+local sidebar = require("interface.sidebar")
 
 local xrandr = require("interface.xrandr")
 
@@ -7,23 +8,28 @@ alt = "Mod1"
 
 -- App keybinding
 awful.keyboard.append_global_keybindings({
-	awful.key({ modkey }, "Return", function()
-		awful.spawn(Apps.terminal) -- Alacritty
-	end, { description = "open alacritty", group = "launcher" }),
-	awful.key({ alt }, "c", function()
-		awful.spawn(Apps.browser) -- Firefox
-	end, { description = "open firefox", group = "launcher" }),
-	awful.key({ modkey }, "space", function()
-		awful.spawn(Apps.launcher, false)
-	end), -- Rofi
-	awful.key({ alt }, "t", function()
-		awful.titlebar.toggle(client.focus)
-	end), -- Toggle titlebar
-	awful.key({ alt }, "s", function()
-		awesome.emit_signal("sidebar::toggle")
-	end), -- Toggle sidebar
-	--
-	awful.key({}, "XF86Display", function()
-	    awesome.emit_signal("displays::toggle")	
-	end), -- Set monitors
+    awful.key({ modkey }, "Return", function()
+        awful.spawn(Apps.terminal) -- Alacritty
+    end, { description = "open alacritty", group = "launcher" }),
+    awful.key({ alt }, "c", function()
+        awful.spawn(Apps.browser) -- Firefox
+    end, { description = "open firefox", group = "launcher" }),
+    awful.key({ modkey }, "space", function()
+        awful.spawn(Apps.launcher, false)
+    end), -- Rofi
+    awful.key({ alt }, "t", function()
+        awful.titlebar.toggle(client.focus)
+    end), -- Toggle titlebar
+    awful.key({ alt }, "s", function()
+        awesome.emit_signal("sidebar::toggle")
+    end), -- Toggle sidebar
+    awful.key({}, "Escape", function()
+        if sidebar.visible then
+            awesome.emit_signal("sidebar::toggle")
+        end
+    end), -- Close sidebar
+    --
+    awful.key({}, "XF86Display", function()
+        awesome.emit_signal("displays::toggle")
+    end), -- Set monitors
 })
