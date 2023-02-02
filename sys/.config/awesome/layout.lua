d7b7c541e907d421c1fb3b380dcd965fdce9af5d
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-02-02 22:05:22 +0100

    WS | AWESOME: Reorder layouts for commodity

--- sys/.config/awesome/layout.lua
+++ sys/.config/awesome/layout.lua
@@ -2,8 +2,8 @@ local awful = require("awful")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.spiral.dwindle,
 	awful.layout.suit.tile,
+	awful.layout.suit.spiral.dwindle,
 	awful.layout.suit.floating,
 }
 
