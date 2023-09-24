a4d991d7e081bd7d836fc3e2b6661c2e2c72fc53
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.co>
2023-09-24 19:49:29 +0200

    fix(arch): Fix gdm awesome startup

--- sys/.config/awesome/startup.lua
+++ sys/.config/awesome/startup.lua
@@ -4,8 +4,6 @@ local displays = require("system.displays")
 
 --- Autostart ---
 
-awful.screen.set_auto_dpi_enabled(true)
-
 -- Picom
 run.start_if_not_running("picom", function()
 	awful.spawn("picom --config /home/archy/.config/picom/picom.conf", false)
@@ -14,6 +12,5 @@ end)
 -- Display reset
 displays.update_displays()
 
-
 -- Redshift
-run.restart("redshift", false)
+run.restart("redshift")
