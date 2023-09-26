bdd550fae1c4f3b3254e23321a70713a74827323
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.co>
2023-09-26 19:11:16 +0200

    chore(arch): Update arch installation with audio tools

--- arch-installation/04-audio.sh
+++ arch-installation/04-audio.sh
@@ -3,22 +3,25 @@
 set -e
 
 PKGS=(
-    # https://wiki.archlinux.org/title/Bluetooth
-    pipewire
-    pipewire-alsa
-    pipewire-pulse
-    pipewire-jack
-    qpwgraph
-    wireplumber
-    bluez
-    bluez-utils
-    bluez-hid2hci
+  # https://wiki.archlinux.org/title/Bluetooth
+  pipewire
+  pipewire-alsa
+  pipewire-pulse
+  pipewire-jack
+  qpwgraph
+  wireplumber
+  bluez
+  bluez-utils
+  bluez-hid2hci
+
+  ffmpeg
+  vlc
 )
 
 for PKG in "${PKGS[@]}"; do
-    echo
-    echo "INSTALLING PACKAGE: $PKG"
-    yay -S "$PKG" --noconfirm --needed
+  echo
+  echo "INSTALLING PACKAGE: $PKG"
+  yay -S "$PKG" --noconfirm --needed
 done
 
 echo
