d3b0b608212e16e754b15761472646944fb96479
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.co>
2023-09-30 18:37:17 +0200

    chore(arch): Refactor development and java setup

--- arch-installation/03-development.sh
+++ arch-installation/03-development.sh
@@ -3,57 +3,58 @@
 set -e
 
 PKGS=(
-  # Neovim
-  prettier
-  npm
+	# Neovim
+	npm
 
-  # JVM
-  jre8-openjdk
-  jre11-openjdk
-  jdk11-openjdk
-  jre17-openjdk
-  jdk17-openjdk
-  kotlin
-  gradle
-  groovy
+	# JVM
+	jre8-openjdk
+	jre11-openjdk
+	jre17-openjdk
+	jre-openjdk
+	jdk11-openjdk
+	jdk17-openjdk
+	jdk-openjdk
+	kotlin
+	gradle
+	groovy
 
-  # Lua
-  lua
-  luarocks
-  stylua
+	# Lua
+	lua
+	luarocks
+	stylua
 
-  # Python
-  python
-  python-pip
-  python-virtualenv
-  python-pipenv
-  python-pynvim
-  python-setuptools
-  python-black
-  python-pre-commit
-  pip-tools
-  flake8
+	# Python
+	python
+	python-pip
+	python-virtualenv
+	python-pipenv
+	python-pynvim
+	python-setuptools
+	python-black
+	python-pre-commit
+	pip-tools
+	flake8
 
-  # IDE
-  intellij-idea-community-edition
-  pycharm-community-edition
-  vscodium-bin
+	# IDE
+	intellij-idea-community-edition
+	pycharm-community-edition
+	visual-studio-code-bin
 
-  # Docker
-  docker
-  docker-compose
+	# Docker
+	docker
+	docker-compose
 
-  # Misc
-  jq
-  yq
-  slack-desktop
-  zsa-wally
+	# Misc
+	jq
+	yq
+	slack-desktop
+	zsa-wally
 )
 
 for PKG in "${PKGS[@]}"; do
-  echo
-  echo "INSTALLING PACKAGE: $PKG"
-  yay -S "$PKG" --noconfirm --needed
+	echo
+	echo "INSTALLING PACKAGE: $PKG"
+	yay -S "$PKG" --noconfirm --needed
 done
 
 echo
--- sys/.config/zsh/02-aliases.zsh
+++ sys/.config/zsh/02-aliases.zsh
@@ -20,6 +20,11 @@ alias path="echo -e \"${PATH//:/\\n}\""
 alias btc='bluetoothctl connect'
 alias btd='bluetoothctl disconnect'
 
+#Java
+alias j11="sudo archlinux-java set java-11-openjdk"
+alias j17="sudo archlinux-java set java-17-openjdk"
+alias j21="sudo archlinux-java set java-21-openjdk"
+
 # Handlr - replacement for xdg-open
 alias open='handlr open'
 
