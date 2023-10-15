f3e480bcce36fb70986ee4f215e18039f263a884
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-10-15 13:43:39 +0200

    fix: Refactor gnome installation

--- installation/arch/01-gnome.sh
+++ installation/arch/01-gnome.sh
@@ -3,22 +3,27 @@
 set -e
 
 PKGS=(
-  gnome
-  gnome-extra
-  gdm-settings
-  gnome-software
+	gnome
+	gnome-extra
+	gnome-software
 
-  # GTK Themes
-  gtk-engine-murrine
-  gtk-engines
-  qogir-gtk-theme
+	# GTK Themes
+	gtk-engine-murrine
+	gtk-engines
+	qogir-gtk-theme
 )
 
 for PKG in "${PKGS[@]}"; do
-  echo
-  echo "INSTALLING PACKAGE: $PKG"
-  yay -S "$PKG" --noconfirm --needed
+	echo
+	echo "INSTALLING PACKAGE: $PKG"
+	yay -S "$PKG" --noconfirm --needed
 done
+
+# gmd is required for gnome, but conflicts with gdm-prime, which is required for prime usage
+
+yay -Rs gdm --noconfirm
+yay -S gdm-prime --noconfirm --needed
+yay -S gdm-settings --noconfirm --needed
 
 echo
 echo "Installation is done"
