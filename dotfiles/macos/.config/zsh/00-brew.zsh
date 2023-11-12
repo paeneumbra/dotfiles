275eb86ddde6fa032e8f0ea5a8b817c7b5a45ca2
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-11-12 23:28:55 +0100

    chore: Update paths for macos

--- dotfiles/macos/.config/zsh/00-brew.zsh
+++ dotfiles/macos/.config/zsh/00-brew.zsh
@@ -1,7 +1,9 @@
 # Brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-export PATH=/opt/homebrew/bin:$PATH
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="/usr/bin:$PATH"
 export HOMEBREW_NO_ANALYTICS=1
 
 export JAVA_HOME="$(/usr/libexec/java_home -v 17)"
