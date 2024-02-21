58c5694fb5aba13faf58a7672821b38585ee34df
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2024-02-22 00:17:22 +0100

    chore: Continue the refactor of fzf functions

--- dotfiles/macos/.config/zsh/08-functions.zsh
+++ dotfiles/macos/.config/zsh/08-functions.zsh
@@ -2,7 +2,7 @@
 function zd() {
 	local dir
 	dir=$(zoxide query -l "$1" | fzf)
-	z "$dir" || exit
+	cd "$dir" || exit
 }
 
 # Find file and move to directory with fzf
@@ -11,21 +11,22 @@ function zfiledir() {
 	local dir
 	file=$(fzf -q "$1")
 	dir=$(dirname "$file")
-	z "$dir" || exit
+	cd "$dir" || exit
 }
 
 # Find command with fzf, pressing `ESC` aborts with the modified or incomplete command
 function fcmd() {
-	print -z "$(fc -rnl 1 | fzf -q "$1" --bind esc:print-query+abort)"
+	print -z "$(fc -rnl 1 | fzf -q "$1" --prompt 'ESC to keep modified cmd> ' --bind esc:print-query+abort)"
 }
 
 # Print envs with fzf
 function envs() {
-	printenv | fzf
+	printenv | fzf -q "$1"
 }
 
+# Find in file with fzf, rg and bat
 function fif() {
-	RG_PREFIX="rg --column --line-number --no-heading --color=always --hidden --glob=!.git --smart-case "
+	RG_PREFIX="rg --column --line-number --no-heading --color=always --hidden --glob='!.git' --smart-case "
 	INITIAL_QUERY="${*:-}"
 	: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
 		--prompt 'Find in file> ' \
@@ -37,6 +38,7 @@ function fif() {
 		--bind 'enter:become(nvim {1} +{2})'
 }
 
+# find file or directory with fzf - does not work as expected
 function fzfind() {
 	fzf --prompt 'Files> ' \
 		--header 'CTRL-T: Switch between Files/Directories' \
@@ -47,6 +49,7 @@ function fzfind() {
 		--bind 'enter:become(nvim {1})'
 }
 
+# Find file with fd, fzf and open in nvim
 function ffile() {
 	FD_PREFIX="fd --hidden --no-ignore --ignore-case --type file"
 	INITIAL_QUERY="${*:-}"
@@ -59,14 +62,45 @@ function ffile() {
 		--bind 'enter:become(nvim {1})'
 }
 
+# Find directory and move to it or copy to clipboard
 function fdir() {
 	FD_PREFIX="fd --hidden --no-ignore --ignore-case --type directory"
 	INITIAL_QUERY="${*:-}"
-	: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
-		--prompt 'Directory> ' \
+	DIR="$(: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+		--prompt 'Directory (ctrl-y to clipboard) > ' \
 		--bind "start:reload:$FD_PREFIX {q}" \
 		--bind "change:reload:sleep 0.1; $FD_PREFIX {q} || true" \
 		--delimiter : \
 		--preview 'tree -C {}' \
-		--bind 'enter:become(zoxide_z {1})'
+		--bind 'ctrl-y:execute-silent(echo {} | pbcopy)+abort')"
+	cd "$DIR" || exit
+}
+
+# Find by extension with fd, fzf and open in nvim
+function fde() {
+	FD_PREFIX="fd --hidden --no-ignore --ignore-case --extension"
+	INITIAL_QUERY="${*:-}"
+	: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+		--prompt 'Extention> ' \
+		--bind "start:reload:$FD_PREFIX {q}" \
+		--bind "change:reload:sleep 0.1; $FD_PREFIX {q} || true" \
+		--delimiter : \
+		--preview 'bat --color=always {}' \
+		--bind 'enter:become(nvim {1})'
+}
+
+# Find directory and open it with IDE
+function zide() {
+	FD_PREFIX="zoxide query -l "
+	INITIAL_QUERY="${*:-}"
+	: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+		--prompt 'enter idea, ctrl-n nvim, ctrl-o code, ctrl-y pycharm> ' \
+		--bind "start:reload:$FD_PREFIX {q}" \
+		--bind "change:reload:sleep 0.1; $FD_PREFIX {q} || true" \
+		--delimiter : \
+		--preview 'tree -C {}' \
+		--bind 'enter:become(idea {1})' \
+		--bind 'ctrl-n:become(nvim {1})' \
+		--bind 'ctrl-o:become(code {1})' \
+		--bind 'ctrl-y:become(pycharm {1})'
 }
