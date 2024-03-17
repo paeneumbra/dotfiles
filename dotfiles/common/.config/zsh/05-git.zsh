f88d0e3f377e26e46e5f09ef71c718ff07b00fe9
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2024-03-17 15:57:07 +0100

    chore: refresh git alias config

--- dotfiles/common/.config/zsh/05-git.zsh
+++ dotfiles/common/.config/zsh/05-git.zsh
@@ -6,34 +6,57 @@ git_version="${${(As: :)$(git version 2>/dev/null)}[3]}"
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
   local ref
-  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,master}; do
     if command git show-ref -q --verify $ref; then
       echo ${ref:t}
-      return
+      return 0
     fi
   done
+
+  # If no main branch was found, fall back to master but return error
   echo master
+  return 1
 }
 
-# Git
+# Rename local branch
+function gbrename() {
+  if [[ -z "$1" || -z "$2" ]]; then
+    echo "Use: $0 old_branch_name new_branch_name"
+    return 1
+  fi
+  git branch -m "$1" "$2"
+}
+
 alias ga='git add'
-alias gst='git status'
-alias gcm='git checkout $(git_main_branch)'
-alias gcmsg='git commit -m'
-alias gcb='git checkout -b'
-alias gco='git checkout'
+alias gap='git add -p'
 alias gaa='git add --all'
-alias glo='git log --oneline --decorate'
+alias gc!='git commit --verbose --amend'
+alias gcb='git checkout -b'
+alias gcf='git config --list'
+#alias gcm='git checkout $(git_main_branch)'
+alias gcm='git switch $(git_main_branch)'
+alias gcmsg='deprecated gcmsg gm'
+#alias gco='git checkout'
+alias gco='git switch'
 alias gema='git log --pretty="%an - %ae"'
 alias glgg='git log --graph'
+alias glo='git log --oneline --decorate --graph'
+alias gloa='git log --oneline --decorate --graph --all'
+alias gm='git commit -m'
+alias gmb='git branch --merged | grep -Ev "(^\*|^\s*(master|main)$)"'
+alias gsl='git stash list'
+alias gsp='git stash pop'
+alias gst='git status'
+alias gsu='git stash -u -m'
 
-# Commands still trying to memorize
-# More: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh
-alias gc!='git commit --verbose --amend'
-alias gcf='git config --list'
-alias gd='git diff'
+# Tryout
+alias gpr='git pull --rebase -v'
+alias glb='git branch -vv'
+alias gub='git branch -vv --no-merged'
 
-# Deprecated
-alias gsab='git submodule add -b'
+# Some more ideas: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh
 
+function deprecated(){
+  print -Pu2 "%F{red}$1%F{yellow} is deprecated in favour of %F{green}$2%F{yellow}"
+}
 unset git_version
