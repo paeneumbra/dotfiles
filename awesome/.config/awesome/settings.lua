1548887da4b2c909acb3448d9985b7982d883f67
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-08-05 21:07:15 +0200

    chore: Walppapers

--- awesome/.config/awesome/settings.lua
+++ awesome/.config/awesome/settings.lua
@@ -42,7 +42,7 @@ Color = {
 
 -- TODO: Use gears.filesystem.file_readable (filename) to check is wallpaper exists
 -- if not, default, or send an error
-Wallpaper = os.getenv "HOME" .. "/workspace/wallpapers/red-samurai.png"
+Wallpaper = os.getenv "HOME" .. "/workspace/wallpapers/feudal-japan.jpg"
 
 Terminal = "wezterm"
 Editor = "nvim"
