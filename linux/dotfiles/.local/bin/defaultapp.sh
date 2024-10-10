d9f018307abc2e08f061bce1208330a98a2e58dc
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-10-10 19:17:51 +0200

    chore: Add wifi script

--- linux/dotfiles/.local/bin/defaultapp.sh
+++ linux/dotfiles/.local/bin/defaultapp.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#        __     ____            ____
+#   ____/ /__  / __/___ ___  __/ / /_   ____ _____  ____
+#  / __  / _ \/ /_/ __ `/ / / / / __/  / __ `/ __ \/ __ \
+# / /_/ /  __/ __/ /_/ / /_/ / / /_   / /_/ / /_/ / /_/ /
+# \__,_/\___/_/  \__,_/\__,_/_/\__/   \__,_/ .___/ .___/
+#                                         /_/   /_/
+
+# Define the default app for opening a file type
+
 FILETYPE=$(xdg-mime query filetype $1)
 APP=$(fd -c always --search-path /usr/share -e .desktop --exec basename | rofi -theme base -dmenu -i -p "Default to")
 xdg-mime default "$APP" "$FILETYPE"
