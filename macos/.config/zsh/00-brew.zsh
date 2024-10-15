11cf272112ef33addbebdf87dfc181125d6ad95c
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-10-15 21:55:30 +0200

    chore: tweak zsh configuration

--- macos/.config/zsh/00-brew.zsh
+++ macos/.config/zsh/00-brew.zsh
@@ -4,3 +4,7 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 export HOMEBREW_NO_ANALYTICS=1
+
+# Python is not recognized after macos is updated
+# Can cause apps to break
+alias python="python3"
