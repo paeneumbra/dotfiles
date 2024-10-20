25ffc4de572e333772d30d77263b68d636340f0b
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-10-20 02:06:27 +0200

    chore: add taskfile
    chore: Update taskfiles for linux, WIP

--- macos/.config/zsh/00-brew.zsh
+++ macos/.config/zsh/00-brew.zsh
@@ -5,7 +5,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 export HOMEBREW_NO_ANALYTICS=1
 
-export PATH="/opt/homebrew/bin/python3:$PATH"
-export PATH="/opt/homebrew/bin/pip3:$PATH"
-export PATH="$HOME/foundry/macos/.local/bin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
+#export PATH="/opt/homebrew/bin/python3:$PATH"
+#export PATH="/opt/homebrew/bin/pip3:$PATH"
+#export PATH="$HOME/foundry/macos/.local/bin:$PATH"
+#export PATH="/opt/homebrew/bin:$PATH"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - path)"
