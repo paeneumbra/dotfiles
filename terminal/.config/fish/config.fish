cd778267abc6f9ec03563e9bf6eae10809daa03a
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-15 17:16:50 +0100

    chore: tweak fish configurations

--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -6,6 +6,9 @@ if test "$os" = "darwin"
     fish_add_path /opt/homebrew/bin
 end
 
+set --universal --export PYENV_ROOT $HOME/.pyenv
+fish_add_path --path $PYENV_ROOT/bin
+
 # End of file required
 starship init fish | source
 
