db4899419b2efddcf32c9494eb080ddfbe5abbb0
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-02-14 11:49:45 +0100

    chore: update python installation

--- anvil/tests/settings_test.py
+++ anvil/tests/settings_test.py
@@ -8,6 +8,6 @@ class TestSettings:
     def test_os(self):
         assert OS == "Darwin" or OS == "Linux"
 
-    def test_distro(self):
-        if OS == "Darwin":
-            assert DISTRO == "Mac"
+    # def test_distro(self):
+    #     if OS == "Darwin":
+    #         assert DISTRO == "Mac"
