f8b9f26cca7c6acbc413907e8440ed0075c62979
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-02-17 16:21:17 +0100

    chore: another tryout

--- terminal/.config/fish/conf.d/alias-macos.fish
+++ terminal/.config/fish/conf.d/alias-macos.fish
@@ -1,5 +1,5 @@
 switch (uname)
     case Darwin
         alias t='task'
-        alias rssh='ssh-add -D; eval (ssh-agent -c)'
+        alias rssh='ssh-add -D; ssh-agent -k; eval (ssh-agent -c)'
 end
