592aee8d246f1a9ff42b39dc1a96fce493622e1c
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-03-22 23:03:33 +0100

    chore: tweak git branch switch

--- terminal/.config/fish/functions/gs.fish
+++
@@ -1,3 +0,0 @@
-function gs -d 'git switch/checkout branch using fzf'
-    git for-each-ref --format='%(refname:short)' refs/heads | fzf --height 10% --layout=reverse --select-1 | xargs git switch
-end
