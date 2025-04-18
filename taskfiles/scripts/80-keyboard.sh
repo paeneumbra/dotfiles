3e2461cb808d9f106581ad543304dd356b833222
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-04-18 17:15:28 +0200

    chore: dotcachy cleanup

--- taskfiles/scripts/80-keyboard.sh
+++ taskfiles/scripts/80-keyboard.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 
 echo "Enabling keychron keyboard"
 echo "https://github.com/adam-savard/keyboard-function-keys-linux"
-sudo cp "$HOME/foundry/dotcachy/taskfiles/scripts/keychron.service" /etc/systemd/system/
+sudo cp "$HOME/foundry/anvil/taskfiles/scripts/keychron.service" /etc/systemd/system/
 sudo systemctl enable --now keychron.service
 
 echo "Keyboard setup is complete"
