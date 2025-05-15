b2ade5f092da53acd175c5862b03cc69141b9120
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-05-15 16:07:44 +0200

    chore: remove handlr alias

--- terminal/.config/shell/alias-linux.sh
+++ terminal/.config/shell/alias-linux.sh
@@ -4,6 +4,5 @@ if [[ $(uname) == "Linux" ]]; then
   alias t='go-task'
   alias btc='bluetoothctl connect'
   alias btd='bluetoothctl disconnect'
-  alias open='handlr open'
   alias k2='sudo systemctl start keychron'
 fi
